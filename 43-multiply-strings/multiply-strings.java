3bc87c42f8207fed55408a90dc1da78f7c49a593
Mohd Ajeem <149220624+mohdajeem@users.noreply.github.com>
2025-01-14 23:07:30 +0530

    Time: 3 ms (58.05%) | Memory: 42.4 MB (50.09%) - LeetSync

---
+++ 43-multiply-strings/multiply-strings.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String multiply(String num1, String num2) {
+        if(num1.equals("0") || num2.equals("0")) return "0";
+        int n1 = num1.length();
+        int n2 = num2.length();
+
+        int[] result = new int[n1+n2];
+        for(int i = n1-1;i>=0;i--){
+            for(int j = n2-1;j>=0; j--){
+                int mul = (num1.charAt(i)-'0')*(num2.charAt(j)-'0');
+                int sum = mul + result[i+j+1];
+                result[i+j+1] = sum%10;
+                result[i+j]+=sum/10;
+            }
+        }
+
+        // convert result array into string
+        StringBuilder sb = new StringBuilder();
+        for(int res : result){
+            if(!(sb.length() == 0 && res == 0))
+                sb.append(res);
+        }
+        return sb.toString();
+    }
+}
