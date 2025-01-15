e44ccd4354eccd170255506d0e60fcd6b17a1a3d
Mohd Ajeem <149220624+mohdajeem@users.noreply.github.com>
2025-01-15 20:32:21 +0530

    Time: 31 ms (5.31%) | Memory: 55.2 MB (65.90%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.java
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.java
@@ -1,11 +1,28 @@
+import java.util.*;
 class Solution {
     public List<Integer> findDuplicates(int[] nums) {
-        Arrays.sort(nums);
+        // Arrays.sort(nums);
+        // List<Integer> ans = new ArrayList<>();
+        // for(int i = 0;i<nums.length-1;i++){
+        //     if(nums[i] == nums[i+1]){
+        //         ans.add(nums[i]);
+        //         i++;
+        //     }
+        // }
+        // return ans;
+        // hashmap<value, count>
+        Hashtable<Integer, Integer> table = new Hashtable<>();
+        for(int i = 0;i<nums.length;i++){
+            if(table.containsKey(nums[i])){
+                table.put(nums[i], table.get(nums[i])+1);
+            } else{
+                table.put(nums[i], 1);
+            }
+        }
         List<Integer> ans = new ArrayList<>();
-        for(int i = 0;i<nums.length-1;i++){
-            if(nums[i] == nums[i+1]){
-                ans.add(nums[i]);
-                i++;
+        for(Map.Entry<Integer, Integer> val: table.entrySet()){
+            if(val.getValue() == 2){
+                ans.add(val.getKey());
             }
         }
         return ans;
