1b56bec2cc0261493152830931934895a2ee1e82
Mohd Ajeem <149220624+mohdajeem@users.noreply.github.com>
2025-01-21 15:35:32 +0530

    Time: 4 ms (15.27%) | Memory: 44.9 MB (30.43%) - LeetSync

---
+++ 695-max-area-of-island/max-area-of-island.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int maxAreaOfIsland(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        boolean[][] vis = new boolean[n][m];
+        int maxArea = 0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(!vis[i][j] && grid[i][j] == 1){
+                    maxArea = Math.max(maxArea, bfs(grid, i, j, vis));
+                }
+            }
+        }
+        return maxArea;
+        
+    }
+    public int bfs(int[][] grid, int sr, int sc, boolean[][] vis){
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(sr,sc));
+        int countArea = 1;
+        vis[sr][sc]=true;
+        while(!q.isEmpty()){
+            Pair p = q.poll();
+            int row = p.row;
+            int col = p.col;
+            int[] drow = {0,-1,0,1};
+            int[] dcol = {1,0,-1,0};
+            for(int i = 0;i<4;i++){
+                int nrow = row+drow[i];
+                int ncol = col+dcol[i];
+                if(nrow>=0 && nrow<grid.length && ncol>=0 && ncol<grid[0].length && !vis[nrow][ncol] && grid[nrow][ncol] == 1){
+                    countArea++;
+                    vis[nrow][ncol] = true;
+                    q.add(new Pair(nrow, ncol));
+                }
+            }
+        }
+        return countArea;
+    }
+}
+class Pair{
+    int row;
+    int col;
+    public Pair(int row, int col){
+        this.row = row;
+        this.col = col;
+    }
+}
