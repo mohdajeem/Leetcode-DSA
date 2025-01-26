c48fc3d2d2d08ccc6b257693788f6232395cd30e
Mohd Ajeem <149220624+mohdajeem@users.noreply.github.com>
2025-01-26 23:13:20 +0530

    Time: 1 ms (74.15%) | Memory: 45.8 MB (29.68%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -5,10 +5,6 @@ class Solution {
         boolean[][] vis = new boolean[n][m];
         for(int i =0;i<n;i++){
             for(int j =0;j<m;j++){
-                // if(matrix[i][j] == 0 && !vis[i][j]){
-                //     makeZero(matrix, i, j,vis);
-                //     break;
-                // }
                 if(matrix[i][j] == 0) vis[i][j] = true;
             }
         }
@@ -29,27 +25,5 @@ class Solution {
         for(int z = 0;z<matrix.length;z++){
             matrix[z][j] = 0;
         }
-
-
-
-
-
-
-
-
-        // // make full row zero
-        // for(int z = 0;z<matrix[0].length;z++){
-        //     if(matrix[i][z]!=0 && !vis[i][z]){
-        //         vis[i][z] = true;
-        //         matrix[i][z] = 0;
-        //     }
-        // }
-        // // make row with j will be zero
-        // for(int z =0;z<matrix.length;z++){
-        //     if(matrix[z][j] != 0 && !vis[z][i]){
-        //         vis[z][j] = true;
-        //         matrix[z][j] = 0;
-        //     }
-        // }
     }
 }
