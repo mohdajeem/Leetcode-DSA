5fe0182a852cc0b23daa36302e7b7a79e1a1f49a
Mohd Ajeem <149220624+mohdajeem@users.noreply.github.com>
2025-02-01 13:45:24 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (80.02%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -9,6 +9,28 @@ class Solution {
         }
         return useMemo(m-1, n-1, obstacleGrid, dp);
     }
+    // using tabuation 
+    public int useTabluation(int m, int n, int[][] arr){
+        int[][] dp = new int[m][n];
+
+        for(int i = 0;i<m;i++){
+            for(int j = 0;j<n;j++){
+                if(i == 0 && j == 0 && arr[i][j] != 1){
+                    dp[0][0] = 1;
+                } else if(arr[i][j] != 1){
+                    int up = 0;
+                    if(i>0) up = arr[i-1][j];
+                    int left = 0;
+                    if(j >0) left = arr[i][j-1];
+                    dp[i][j] = up+left;
+                }
+            }
+        }
+        return dp[m-1][n-1];
+    }
+
+    // using memoization
+
     public int useMemo(int i, int j, int[][] arr, int[][] dp){
         if(i < 0 || j < 0 || arr[i][j] == 1) return 0;
         if(i == 0 && j == 0) return 1;
