43da898ab385f7b9baca8be0191f31c5ca639da1
Mohd Ajeem <149220624+mohdajeem@users.noreply.github.com>
2025-02-01 14:09:03 +0530

    Time: 1 ms (20.42%) | Memory: 41.6 MB (80.02%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -7,11 +7,32 @@ class Solution {
         for(int[] ar : dp){
             Arrays.fill(ar, -1);
         }
-        return useMemo(m-1, n-1, obstacleGrid, dp);
+        // return useMemo(m-1, n-1, obstacleGrid, dp);
         // return useTabluation(m,n,obstacleGrid);
+        return useTabluation2(m,n,obstacleGrid);
     }
-    // optimization 
-    // public int useOptimization
+
+    public int useTabluation2(int m, int n, int[][] arr){
+        int[][] dp = new int[m][n];
+        if(arr[0][0] == 1) return 0;
+
+        for(int i = 0;i<m;i++){
+            for(int j = 0;j<n;j++){
+                if(arr[i][j] == 1) dp[i][j] = 0;
+                else if(i == 0 && j == 0) dp[i][j] = 1;
+                else{
+                    int up = 0;
+                    if(i >0) up = dp[i-1][j];
+                    int left = 0;
+                    if(j>0) left = dp[i][j-1];
+                    dp[i][j] = up+left;
+                }
+                
+            }
+        }
+        return dp[m-1][n-1];
+    }
+
 
     // using tabuation 
     public int useTabluation(int m, int n, int[][] arr){
