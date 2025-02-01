b4489b52e608c55324c3204690226ba5458e3174
Mohd Ajeem <149220624+mohdajeem@users.noreply.github.com>
2025-02-01 13:19:41 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (17.52%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,12 +1,37 @@
 class Solution {
     public int uniquePaths(int m, int n) {
         // return useRec(m-1,n-1);
+        /*
         int[][] dp = new int[m][n];
         for(int[] arr : dp){
             Arrays.fill(arr, -1);
         }
         return useMemo(m-1, n-1, dp);
+
+        */
+
+        // use Tabulation method for optimisation
+        return useTab(m,n);
     }
+    public int useTab(int m, int n){
+        int[][] dp = new int[m][n];
+        for(int i = 0;i<m;i++){
+            for(int j = 0;j<n;j++){
+                if(i == 0 && j == 0){
+                    dp[0][0] = 1;
+                    continue;
+                }
+                int up = 0;
+                if(i > 0) up = dp[i-1][j];
+                int left = 0;
+                if(j>0) left = dp[i][j-1];
+                dp[i][j] = up+left;
+            }
+        }
+        return dp[m-1][n-1];
+    }
+
+
     public int useMemo(int m, int n, int[][] dp){
         if(m == 0 && n == 0) return 1;
         if(m < 0 || n < 0) return 0;
