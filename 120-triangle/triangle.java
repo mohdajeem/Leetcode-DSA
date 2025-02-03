efea4c375125a080256328b2d794b7ee8fe1b2e4
Mohd Ajeem <149220624+mohdajeem@users.noreply.github.com>
2025-02-03 15:28:39 +0530

    Time: 4 ms (49.00%) | Memory: 45.4 MB (12.06%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -9,11 +9,12 @@ class Solution {
             }
             i++;
         }
-        int[][] dp = new int[n][n];
-        for(int[] arr : dp){
-            Arrays.fill(arr,-1);
-        }
-        return min(0,0,trian,dp);
+        // int[][] dp = new int[n][n];
+        // for(int[] arr : dp){
+        //     Arrays.fill(arr,-1);
+        // }
+        // return min(0,0,trian,dp);
+        return tab(trian);
     }
     public int min(int i, int j, int[][] trian, int[][] dp){
         if(i == trian.length-1) return trian[i][j];
@@ -22,5 +23,24 @@ class Solution {
         int diag = min(i+1, j+1, trian,dp);
 
         return dp[i][j] = trian[i][j] + Math.min(down, diag);
+    }
+    public int tab(int[][] trian){
+        int n = trian.length;
+        int[][] dp = new int[n][n];
+
+        // base case from the recursion
+        for(int j = 0;j<n;j++){
+            dp[n-1][j]=trian[n-1][j];
+        }
+        for(int i = n-2;i>=0;i--){
+            for(int j = 0;j<=i;j++){
+                int down = dp[i+1][j];
+                int diag = dp[i+1][j+1];
+                dp[i][j] = trian[i][j] + Math.min(down, diag);
+            }
+        }
+        return dp[0][0];
+
+
     }
 }
