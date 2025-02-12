c8b2949bfc93902942ca059b734e11f4f72300cc
Mohd Ajeem <149220624+mohdajeem@users.noreply.github.com>
2025-02-12 12:42:34 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (66.74%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
@@ -1,25 +1,34 @@
 class Solution {
     public boolean check(int[] arr) {
         int n = arr.length;
-        int[] A = new int[n];
+        int c = 0;
         for(int i = 0;i<n;i++){
-            A[i] = arr[i];
+            if(arr[i] > arr[(i+1)%n]) c++;
         }
-        Arrays.sort(A);
-        int k = 0;
-        int j = 0;
-        for(int i = 0;i<arr.length-1;i++){
-            if(arr[i] > arr[i+1]){
-                j = i+1;
-                break;
-            }
-        }
-        for(int i = j;j<n;j++){
-            if(A[k] != arr[(k+i)%n]){
-                return false;
-            }
-            k++;
-        }
-        return true;
+        return c<=1;
+
+
+
+        // int n = arr.length;
+        // int[] A = new int[n];
+        // for(int i = 0;i<n;i++){
+        //     A[i] = arr[i];
+        // }
+        // Arrays.sort(A);
+        // int k = 0;
+        // int j = 0;
+        // for(int i = 0;i<arr.length-1;i++){
+        //     if(arr[i] > arr[i+1]){
+        //         j = i+1;
+        //         break;
+        //     }
+        // }
+        // for(int i = j;j<n;j++){
+        //     if(A[k] != arr[(k+i)%n]){
+        //         return false;
+        //     }
+        //     k++;
+        // }
+        // return true;
     }
 }
