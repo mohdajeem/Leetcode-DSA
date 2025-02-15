e66a82159e6d7d27117cc291215b46d79b7b0d47
Mohd Ajeem <149220624+mohdajeem@users.noreply.github.com>
2025-02-16 04:47:36 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (28.70%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,46 +1,26 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int n=nums.length;
-        int break_point = 0;
-        for(int i =0;i<n-1;i++){
-            if(nums[i]>nums[i+1]){
-                break_point = i+1;
-                break;
-            }
-        }
-        int ans = -1;
-        System.out.println(break_point);
-        if(target <= nums[n-1]){
-            // our target would from break_point to end;
-            System.out.println("inside t<nums[n-1]");
-            int s = break_point;
-            int e = n-1;
-            while(s<=e){
-                int mid = s+(e-s)/2;
-                if(nums[mid] == target){
-                    return mid;
-                } else if(nums[mid] > target){
+        int n = nums.length;
+        int s = 0, e = n-1;
+        while(s<=e){
+            int mid = s+(e-s)/2;
+            if(nums[mid] == target) return mid;
+            else if(nums[mid] >= nums[s]){
+                // left side me hoga mid
+                if(target <= nums[mid] && target>=nums[s]){
                     e = mid-1;
-                } else{
+                } else {
                     s = mid+1;
                 }
-            }
-        } else{
-            System.out.println("Else functioon");
-            int s = 0;
-            int e = break_point-1;
-            while(s<=e){
-                int mid = s+(e-s)/2;
-                if(nums[mid] == target){
-                    return mid;
-                } else if(nums[mid]>target){
-                    e = mid-1;
+            } else{
+                // right side me hoga mid
+                if(target >= nums[mid] && target<=nums[e]){
+                    s=mid+1;
                 } else{
-                    s = mid+1;
+                    e=mid-1;
                 }
             }
         }
-
-        return ans;
+        return -1;
     }
 }
