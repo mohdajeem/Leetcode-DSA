2530479aef5593190a5bd7f2bccb5cddf8cbfe79
Mohd Ajeem <149220624+mohdajeem@users.noreply.github.com>
2025-03-07 23:44:20 +0530

    Time: 1102 ms (5.60%) | Memory: 45.5 MB (54.80%) - LeetSync

--- 2610-closest-prime-numbers-in-range/closest-prime-numbers-in-range.java
+++ 2610-closest-prime-numbers-in-range/closest-prime-numbers-in-range.java
@@ -20,7 +20,7 @@ class Solution {
     }
     public boolean isPrime(int n){
         if(n<2) return false;
-        for(int i=2;i<=Math.sqrt(n);i++){
+        for(int i=2;i*i<=n;i++){
             if(n%i==0) return false;
         }
         return true;
