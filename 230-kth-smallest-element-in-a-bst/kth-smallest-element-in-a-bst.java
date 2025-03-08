0e2896c91a947190f8b860cd538852be76dc0a8d
Mohd Ajeem <149220624+mohdajeem@users.noreply.github.com>
2025-03-08 22:03:48 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (23.47%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.java
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.java
@@ -15,14 +15,30 @@
  */
 class Solution {
     public int kthSmallest(TreeNode root, int k) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        preorder(root,pq);
-        int ans = 0;
-        while(k!=0){
-            ans = pq.remove();
-            k--;
+        // PriorityQueue<Integer> pq = new PriorityQueue<>();
+        // preorder(root,pq);
+        // int ans = 0;
+        // while(k!=0){
+        //     ans = pq.remove();
+        //     k--;
+        // }
+        // return ans;
+        int[] ans = new int[1];
+        int[] c = new int[1];
+        recPre(root,k,c,ans);
+        return ans[0];
+    }
+
+    // it is not working I don't know why
+    public void recPre(TreeNode root, int k, int[] c,int[] ans){
+        if(root == null) return;
+        recPre(root.left, k,c,ans);
+        c[0]++;
+        if(c[0]==k){
+            ans[0] = root.val;
+            return;
         }
-        return ans;
+        recPre(root.right, k,c,ans);
     }
     public void preorder(TreeNode root, PriorityQueue<Integer> pq){
         if(root == null){
