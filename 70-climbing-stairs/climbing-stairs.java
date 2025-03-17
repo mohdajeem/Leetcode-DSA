1c7f8fb168db480454f53f7b5f361a614a365ee8
Mohd Ajeem <149220624+mohdajeem@users.noreply.github.com>
2025-03-17 13:23:29 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (65.32%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,35 +1,17 @@
 class Solution {
     public int climbStairs(int n) {
-        // return countStairs(n, new int[n+1]);
-        return countStairsTab(n);
-        
-    }
-    public int countStairs(int n, int[] qb){
-        if(n == 0){
-            return 1;
-        } else if(n < 0){
-            return 0;
-        }
-
-        if(qb[n] > 0){
-            return qb[n];
-        }
-        int nm1 = countStairs(n-1,qb);
-        int nm2 = countStairs(n-2, qb);
-        int cb = nm1+nm2;
-        qb[n] = cb;
-        return cb;
-    }
-    public int countStairsTab(int n){
         int[] dp = new int[n+1];
-        dp[0] = 1;
-        for(int i = 1;i<=n;i++){
-            if(i == 1){
-                dp[i] = 1;
-            } else{
-                dp[i] = dp[i-1]+dp[i-2];
-            }
+        Arrays.fill(dp,-1);
+        return climbRec(n,dp);
+    }
+    public int climbRec(int step,int[] dp){
+        if(step<0) return 0;
+        if(step == 0){
+            return 1;
         }
-        return dp[n];
+        if(dp[step] != -1) return dp[step];
+        int oneStep = climbRec(step -1,dp);
+        int twoStep = climbRec(step-2,dp);
+        return dp[step]=twoStep+oneStep;
     }
 }
