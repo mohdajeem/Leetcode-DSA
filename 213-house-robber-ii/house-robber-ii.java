e0bf92e273685cbdda66f0711af2ccc3cfdfa22e
Mohd Ajeem <149220624+mohdajeem@users.noreply.github.com>
2025-03-22 17:11:24 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (79.81%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,23 +1,21 @@
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
-        if(n == 1) return nums[0];
-        if(n == 2) return Math.max(nums[0],nums[1]);
-        int[] dp1 = new int[n]; 
+        if(n==1) return nums[0];
+        if(n==2) return Math.max(nums[0],nums[1]);
+        int[] dp1 = new int[n];
+        int[] dp2 = new int[n];
         Arrays.fill(dp1,-1);
-        int ans1 = solve(n-1,nums,1,dp1);
-        Arrays.fill(dp1,-1);
-        int ans2 = solve(n-2,nums,0,dp1);
-        return Math.max(ans1, ans2);
+        Arrays.fill(dp2,-1);
+        return Math.max(rec(n-1,1,nums,dp1),rec(n-2,0,nums,dp2));
     }
+    public int rec(int ind, int startInd, int[] nums,int[] dp){
+        if(ind == startInd) return nums[startInd];
+        if(ind < startInd) return 0;
+        if(dp[ind] != -1) return dp[ind];
+        int notTake = rec(ind-1,startInd, nums,dp);
+        int take = nums[ind] + rec(ind-2, startInd, nums,dp);
 
-    public int solve(int ind, int[] nums, int start,int[] dp1){
-        if(ind == start) return nums[start];
-        if(ind < start) return 0;
-        if(dp1[ind] != -1) return dp1[ind];
-        int lh = nums[ind] + solve(ind-2, nums, start, dp1);
-        int rh = solve(ind-1, nums,start, dp1);
-
-        return dp1[ind] = Math.max(lh,rh);
+        return dp[ind]= Math.max(take,  notTake);
     }
 }
