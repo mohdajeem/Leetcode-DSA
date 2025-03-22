30dba7d48c2c2d8af312d630b79944f31f3b136e
Mohd Ajeem <149220624+mohdajeem@users.noreply.github.com>
2025-03-22 16:09:47 +0530

    Time: 2 ms (69.92%) | Memory: 43.1 MB (11.79%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,54 +1,59 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        int n = grid.length;
-        int m = grid[0].length;
-        boolean[][] vis= new boolean[n][m];
+        // vis[][] boolean array
+        // public pair class
+        // use bfs - queue usi se iterater karunga
+        // min lena hoga - which will get updated and take min each time
+        int n = grid.length, m = grid[0].length;
+        boolean[][] vis = new boolean[n][m];
         Queue<Pair> q = new LinkedList<>();
-        int c1 = 0;
-        for(int i = 0;i<n;i++){
+        // first iterate over the array and get count of fresh oranges and if orange is rotten add that to queue
+        int fresh = 0;
+        for(int i =0;i<n;i++){
             for(int j = 0;j<m;j++){
-                if(grid[i][j] == 1){
-                    c1++;
-                } else if(grid[i][j] == 2){
-                    vis[i][j] = true;
-                    q.add(new Pair(i,j,0));
-                }
+                if(grid[i][j]==1) fresh++;
+                else if(grid[i][j]==2) q.add(new Pair(i,j,0));
             }
         }
-        int minTime = 0;
+        int min = 0;
         while(!q.isEmpty()){
-            Pair p = q.poll();
+            Pair p = q.remove();
             int row = p.row;
             int col = p.col;
-            int time = p.time;
-            minTime = Math.max(minTime, time);
+            int dis = p.dis;
             int[] drow = {0,-1,0,1};
             int[] dcol = {1,0,-1,0};
             for(int i =0;i<4;i++){
                 int nrow = row+drow[i];
                 int ncol = col+dcol[i];
-                if(nrow>=0 && nrow<grid.length && ncol >= 0 && ncol<grid[0].length && !vis[nrow][ncol] && grid[nrow][ncol] == 1){
+                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && !vis[nrow][ncol] && grid[nrow][ncol] == 1){
                     vis[nrow][ncol] = true;
                     grid[nrow][ncol] = 2;
-                    q.add(new Pair(nrow,ncol,time+1));
-                    c1--;
+                    fresh--;
+                    q.add(new Pair(nrow,ncol,dis+1));
+                    min = Math.max(min, dis+1);
                 }
             }
         }
-
-        return c1 == 0 ? minTime : -1;
+        // int cntF = 0;
+        // for(int i =0;i<n;i++){
+        //     for(int j =0;j<m;j++){
+        //         if(grid[i][j] == 1){
+        //             c++;
+        //         }
+        //     }
+        // }
+        return fresh == 0 ? min:-1;
 
     }
-
 }
-
 class Pair{
     int row;
     int col;
-    int time;
-    public Pair(int row, int col, int time){
+    int dis;
+    public Pair(int row, int col, int dis){
         this.row = row;
         this.col = col;
-        this.time = time;
+        this.dis = dis;
     }
 }
