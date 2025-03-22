8ebc7a817d5c5400bcc2939df42c56372419c494
Mohd Ajeem <149220624+mohdajeem@users.noreply.github.com>
2025-03-22 16:52:41 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (8.60%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -3,16 +3,19 @@ class Solution {
         int n = nums.length;
         int[] dp = new int[n];
         Arrays.fill(dp,-1);
-        return solve(n-1, nums,dp);
+        return rec(n-1,nums,dp);
     }
-    public int solve(int ind, int[] nums, int[] dp){
-        if(ind < 0) return 0;
-        if(ind == 0) return nums[0];
+    public int rec(int ind, int[] nums,int[] dp){
+        if(ind == 0){
+            return nums[ind];
+        }
         if(dp[ind] != -1) return dp[ind];
 
-        int lh = nums[ind] + solve(ind-2,nums,dp);
-        int rh = solve(ind-1,nums,dp);
-
-        return dp[ind]=Math.max(lh,rh);
+        int notTake = rec(ind-1,nums,dp);
+        int take = nums[ind];
+        if(ind > 1){
+            take = nums[ind] + rec(ind-2,nums,dp);
+        }
+        return dp[ind] =  Math.max(notTake, take);
     }
 }
