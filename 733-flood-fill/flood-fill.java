6070c44e5ba8ca01e095459cd9f9bc9a91949764
Mohd Ajeem <149220624+mohdajeem@users.noreply.github.com>
2025-03-23 11:28:40 +0530

    Time: 1 ms (47.56%) | Memory: 45.2 MB (31.63%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,30 +1,32 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        if(color == image[sr][sc]) return image;
-        int element = image[sr][sc];
-        bfs(image, sr, sc, color, element);
-        return image;
-        
-    }
-    public void bfs(int[][] image, int sr, int sc, int color, int element){
+        // boolean[][] vis array
+        // queue store row, col
+        // apply bfs
+        int n = image.length, m = image[0].length;
+        boolean[][] vis = new boolean[n][m];
         Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(sr,sc));
+        int original = image[sr][sc];
         image[sr][sc] = color;
+        q.add(new Pair(sr,sc));
+        vis[sr][sc] = true;
         while(!q.isEmpty()){
-            Pair p = q.poll();
+            Pair p = q.remove();
             int row = p.row;
             int col = p.col;
-            int[] drow = {1,0,-1,0};
-            int[] dcol = {0,-1,0,1};
+            int[] drow = {0,-1,0,1};
+            int[] dcol = {1,0,-1,0};
             for(int i =0;i<4;i++){
-                int nrow = row+drow[i];
-                int ncol = col+dcol[i];
-                if(nrow>=0 && ncol >= 0 && nrow < image.length && ncol < image[0].length && image[nrow][ncol] == element){
+                int nrow = row + drow[i];
+                int ncol = col + dcol[i];
+                if(nrow < n && nrow >= 0 && ncol < m && ncol >=0 && !vis[nrow][ncol] && image[nrow][ncol] == original){
+                    vis[nrow][ncol] = true;
+                    q.add(new Pair(nrow,ncol));
                     image[nrow][ncol] = color;
-                    q.add(new Pair(nrow, ncol));
                 }
             }
         }
+        return image;
     }
 }
 class Pair{
