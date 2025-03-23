e0bef5e0102f8fa964a28b9ea35fda0e8290e6ac
Mohd Ajeem <149220624+mohdajeem@users.noreply.github.com>
2025-03-23 14:12:38 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (38.46%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -1,79 +1,20 @@
 class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        int m = obstacleGrid.length;
-        int n = obstacleGrid[0].length;
-        // return useRec(m-1,n-1,obstacleGrid);
-        int[][] dp = new int[m][n];
+        int n = obstacleGrid.length, m = obstacleGrid[0].length;
+        int[][] dp = new int[n][m];
         for(int[] ar : dp){
-            Arrays.fill(ar, -1);
+            Arrays.fill(ar,-1);
         }
-        return useMemo(m-1, n-1, obstacleGrid, dp);
-        // return useTabluation(m,n,obstacleGrid);
-        // return useTabluation2(m,n,obstacleGrid);
+        return rec(n-1,m-1,obstacleGrid,dp);
     }
+    public int rec(int i, int j, int[][] obs,int[][] dp){
+        if(i<0 || j<0) return 0;
+        if(obs[i][j] == 1) return 0;
+        if(i == 0 && j==0) return 1;
+        if(dp[i][j]!=-1) return dp[i][j];
+        int up = rec(i-1,j,obs,dp);
+        int left = rec(i,j-1,obs,dp);
 
-    public int useTabluation2(int m, int n, int[][] arr){
-        int[][] dp = new int[m][n];
-        if(arr[0][0] == 1) return 0;
-
-        for(int i = 0;i<m;i++){
-            for(int j = 0;j<n;j++){
-                if(arr[i][j] == 1) dp[i][j] = 0;
-                else if(i == 0 && j == 0) dp[i][j] = 1;
-                else{
-                    int up = 0;
-                    if(i >0) up = dp[i-1][j];
-                    int left = 0;
-                    if(j>0) left = dp[i][j-1];
-                    dp[i][j] = up+left;
-                }
-                
-            }
-        }
-        return dp[m-1][n-1];
-    }
-
-
-    // using tabuation 
-    public int useTabluation(int m, int n, int[][] arr){
-        int[][] dp = new int[m][n];
-        if(arr[0][0] == 1) return 0;
-        dp[0][0] = 1;
-        for(int i=1;i<m;i++){
-            dp[i][0] = (arr[i][0] == 0 && dp[i-1][0]==1) ? 1 : 0;
-        }
-        for(int j=1;j<n;j++){
-            dp[0][j] = (arr[0][j] == 0 && dp[0][j-1] == 1) ? 1: 0;
-        }
-
-        for(int i = 1;i<m;i++){
-            for(int j = 1;j<n;j++){
-                if(arr[i][j] != 1){
-                    dp[i][j] = dp[i-1][j]+dp[i][j-1];
-                } else{
-                    dp[i][j] = 0;
-                }
-            }
-        }
-        return dp[m-1][n-1];
-    }
-
-    // using memoization
-
-    public int useMemo(int i, int j, int[][] arr, int[][] dp){
-        if(i < 0 || j < 0 || arr[i][j] == 1) return 0;
-        if(i == 0 && j == 0) return 1;
-
-        if(dp[i][j] != -1) return dp[i][j];
-        int up = useMemo(i-1, j, arr, dp);
-        int left = useMemo(i, j-1, arr, dp);
-        return dp[i][j] = up+left;
-    }
-    public int useRec(int i, int j, int[][] arr){
-        if(i == 0 && j == 0) return 1;
-        if(i < 0 || j < 0 || arr[i][j] == 1) return 0;
-        int up = useRec(i-1,j,arr);
-        int left = useRec(i, j-1,arr);
-        return up+left;
+        return dp[i][j]=up+left;
     }
 }
