19ed3d7e8b356aef877c06138db1da3da2ddd9c5
Mohd Ajeem <149220624+mohdajeem@users.noreply.github.com>
2025-03-25 20:07:33 +0530

    Time: 9 ms (7.90%) | Memory: 45.6 MB (37.71%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -1,77 +1,69 @@
 class Solution {
     public void solve(char[][] board) {
-        int n = board.length;
-        int m = board[0].length;
+        // vis[][] array - boolean
+        // array - track O's ka surrounding
+        // make a function of bfs 
+        int n = board.length, m = board[0].length;
         boolean[][] vis = new boolean[n][m];
-        Queue<Pair> q = new LinkedList<>();
-        // first row
-        for(int j = 0;j<m;j++){
-            if(board[0][j]=='O' && !vis[0][j]){
-                    vis[0][j] = true;
-                    q.add(new Pair(0,j));
-                    bfs(q, vis, board);
+        int[][] arr=new int[n][m];
+        for(int i =0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if((i==0||i==n-1)){
+                    if(!vis[i][j] && board[i][j] == 'O'){
+                        bfs(i,j,n,m,vis,arr,board);
+                    }
+                } else{
+                    if(j==0 || j == m-1){
+                        if(!vis[i][j] && board[i][j]=='O'){
+                            bfs(i,j,n,m,vis,arr,board);
+                        }
+                    }
                 }
-        }
-        // last row
-        for(int j = 0; j<m;j++){
-            if(board[n-1][j]=='O' && !vis[n-1][j]){
-                    vis[n-1][j] = true;
-                    q.add(new Pair(n-1,j));
-                    bfs(q, vis, board);
-                }
-        }
-        // fist colum
-        for(int i = 1;i<n-1;i++){
-            if(board[i][0] == 'O' && !vis[i][0]){
-                vis[i][0] = true;
-                q.add(new Pair(i,0));
-                bfs(q, vis, board);
+                
             }
         }
-        for(int i = 1;i<n-1;i++){
-            if(board[i][m-1]=='O' && !vis[i][m-1]){
-                    vis[i][m-1] = true;
-                    q.add(new Pair(i,m-1));
-                    bfs(q, vis, board);
-            }
+        for(int[] ar:arr){
+            System.out.println(Arrays.toString(ar));
         }
-
-        // now count the replace all O to X
-        for(int i =1;i<n-1;i++){
-            for(int j = 1;j<m-1;j++){
-                if(!vis[i][j] && board[i][j] == 'O'){
-                    board[i][j] = 'X';
+        // now I have arr and board
+        for(int i=0;i<n;i++){
+            for(int j =0;j<m;j++){
+                if(board[i][j] == 'O' && arr[i][j] != 1){
+                    board[i][j]='X';
                 }
             }
         }
-        
     }
-    public void bfs(Queue<Pair> q, boolean[][] vis, char[][] board){
+
+    public void bfs(int i, int j, int n,int m, boolean[][] vis, int[][] arr, char[][] board){
+        Queue<Pair> q = new LinkedList<>();
+        vis[i][j] = true;
+        arr[i][j] = 1;
+        q.add(new Pair(i,j));
         while(!q.isEmpty()){
-            Pair p = q.poll();
+            Pair p = q.remove();
             int row = p.row;
             int col = p.col;
             int[] drow = {0,-1,0,1};
             int[] dcol = {1,0,-1,0};
-            for(int i = 0;i<4;i++){
-                int nrow = row+drow[i];
-                int ncol = col+dcol[i];
-                if(isValid(nrow, ncol, board) && !vis[nrow][ncol] && board[nrow][ncol] == 'O'){
+            for(int z =0;z<4;z++){
+                int nrow = row+drow[z];
+                int ncol = col+dcol[z];
+                if(nrow >=0 && nrow < n && ncol>=0 && ncol < m && !vis[nrow][ncol] && board[nrow][ncol] == 'O'){
                     vis[nrow][ncol] = true;
-                    q.add(new Pair(nrow, ncol));
+                    arr[nrow][ncol] = 1;
+                    q.add(new Pair(nrow,ncol));
                 }
             }
         }
     }
-    public boolean isValid(int row, int col, char[][] board){
-        return row >= 0 && col >= 0 && row < board.length && col < board[0].length;
-    }
 }
+
 class Pair{
     int row;
     int col;
     public Pair(int row, int col){
-        this.row = row;
-        this.col = col;
+        this.row=row;
+        this.col=col;
     }
 }
