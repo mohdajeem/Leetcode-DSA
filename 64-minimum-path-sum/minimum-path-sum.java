60cb8237a8812c104f7a9ba37c25f5350dc6dcdf
Mohd Ajeem <149220624+mohdajeem@users.noreply.github.com>
2025-03-26 14:58:41 +0530

    Time: 5 ms (28.59%) | Memory: 47.6 MB (69.90%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -1,11 +1,30 @@
 class Solution {
     public int minPathSum(int[][] grid) {
         int n = grid.length, m=grid[0].length;
+        // int[][] dp = new int[n][m];
+        // for(int[] ar : dp){
+        //     Arrays.fill(ar,-1);
+        // }
+        // return f(n-1,m-1,grid,dp);
+        return tabu(grid,n,m);
+    }
+    public int tabu(int[][] grid, int n, int m){
         int[][] dp = new int[n][m];
-        for(int[] ar : dp){
-            Arrays.fill(ar,-1);
+        // dp[0][0] = grid[0][0];
+        for(int i=0;i<n;i++){
+            for(int j =0;j<m;j++){
+                if(i==0 && j==0) {
+                    dp[i][j]=grid[0][0];
+                    continue;
+                }
+                int right = Integer.MAX_VALUE;
+                if(j>0) right = grid[i][j] + dp[i][j-1];
+                int down = Integer.MAX_VALUE;
+                if(i>0) down = grid[i][j] + dp[i-1][j];
+                dp[i][j] = Math.min(right,down);
+            }
         }
-        return f(n-1,m-1,grid,dp);
+        return dp[n-1][m-1];
     }
 
     public int f(int i, int j, int[][] grid,int[][] dp){
