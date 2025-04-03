3ad61605d65bd7140e0e3e57609c237e5f393c5a
Mohd Ajeem <149220624+mohdajeem@users.noreply.github.com>
2025-04-03 16:28:20 +0530

    Time: 48 ms (73.68%) | Memory: 147.9 MB (98.12%) - LeetSync

---
+++ 1876-map-of-highest-peak/map-of-highest-peak.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int[][] highestPeak(int[][] isWater) {
+        int n = isWater.length, m = isWater[0].length;
+        Queue<Pair> q = new LinkedList<>();
+        int[][] vis = new int[n][m];
+        for(int i =0;i<n;i++){
+            for(int j = 0;j<m;j++){
+                if(isWater[i][j] == 1){
+                    q.add(new Pair(i,j,0));
+                    vis[i][j] = 0;
+                } else{
+                    vis[i][j] = -1;
+                }
+            }
+        }
+        while(!q.isEmpty()){
+            Pair p = q.remove();
+            int row = p.row;
+            int col = p.col;
+            int dis = p.dis;
+            int[] drow = {0,-1,0,1};
+            int[] dcol = {1,0,-1,0};
+            for(int i =0;i<4;i++){
+                int nrow = row+drow[i];
+                int ncol = col+dcol[i];
+                if(nrow >= 0 && nrow<n && ncol >=0 && ncol<m && vis[nrow][ncol] == -1){
+                    vis[nrow][ncol] = dis+1;
+                    q.add(new Pair(nrow, ncol, dis+1));
+                }
+            }
+
+        }
+        return vis;
+        
+        
+    }
+}
+class Pair{
+    int row;
+    int col;
+    int dis;
+    public Pair(int row, int col, int dis){
+        this.row = row;
+        this.col = col;
+        this.dis = dis;
+    }
+}
