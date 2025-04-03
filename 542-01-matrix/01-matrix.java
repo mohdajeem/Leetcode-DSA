35c534001a84b768c0f64ed8b7d624eb672f2744
Mohd Ajeem <149220624+mohdajeem@users.noreply.github.com>
2025-04-03 16:08:09 +0530

    Time: 13 ms (84.81%) | Memory: 47.2 MB (72.35%) - LeetSync

--- 542-01-matrix/01-matrix.java
+++ 542-01-matrix/01-matrix.java
@@ -1,37 +1,39 @@
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
-        // 2d vis arra int wala
         int n = mat.length, m = mat[0].length;
-        int[][] vis = new int[n][m];
         Queue<Pair> q = new LinkedList<>();
+        int[][] vis = new int[n][m];
+
         for(int i =0;i<n;i++){
-            for(int j =0;j<m;j++){
+            for(int j=0;j<m;j++){
                 if(mat[i][j] == 0){
                     q.add(new Pair(i,j,0));
-                    vis[i][j]=0;
+                    vis[i][j] = 0;
                 }else{
-                    vis[i][j] = -1;
+                    vis[i][j]=-1;
                 }
             }
         }
+
         while(!q.isEmpty()){
             Pair p = q.remove();
             int row = p.row;
             int col = p.col;
             int dis = p.dis;
-            int[] drow = {0,-1,0,1};
-            int[] dcol = {1,0,-1,0};
+            int[] drow={0,-1,0,1};
+            int[] dcol={1,0,-1,0};
             for(int i =0;i<4;i++){
                 int nrow = row+drow[i];
                 int ncol = col+dcol[i];
-                if(nrow>=0 && nrow<n && ncol >=0 && ncol<m && vis[nrow][ncol]==-1){
-                  vis[nrow][ncol] = dis+1;
-                  q.add(new Pair(nrow,ncol, dis+1));
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol] == -1){
+                    vis[nrow][ncol] = dis+1;
+                    q.add(new Pair(nrow, ncol, dis+1));
                 }
             }
         }
 
         return vis;
+
     }
 }
 
@@ -40,8 +42,8 @@ class Pair{
     int col;
     int dis;
     public Pair(int row, int col, int dis){
-        this.row=row;
-        this.col=col;
-        this.dis=dis;
+        this.row = row;
+        this.col = col;
+        this.dis = dis;
     }
 }
