8b771eed080820a03cbb23e041f1ecfa167fbe99
Mohd Ajeem <149220624+mohdajeem@users.noreply.github.com>
2025-05-25 23:24:36 +0530

    Time: 153 ms (11.79%) | Memory: 61.2 MB (6.71%) - LeetSync

--- 2237-longest-palindrome-by-concatenating-two-letter-words/longest-palindrome-by-concatenating-two-letter-words.java
+++ 2237-longest-palindrome-by-concatenating-two-letter-words/longest-palindrome-by-concatenating-two-letter-words.java
@@ -2,32 +2,29 @@ class Solution {
     public int longestPalindrome(String[] words) {
         HashMap<String, Integer> map = new HashMap<>();
         int ans = 0;
-        for(int i =0;i<words.length;i++){
-            map.put(words[i], map.getOrDefault(words[i],0)+1);
+        for(String st : words){
+            char ch1 = st.charAt(0);
+            char ch2 = st.charAt(1);
+            String rev = String.valueOf(ch2)+String.valueOf(ch1);
+            if(map.containsKey(rev) && map.get(rev) > 0){
+                ans+=4;
+                map.put(rev, map.get(rev)-1);
+            } else{
+                map.put(st, map.getOrDefault(st, 0) + 1);
+
+            }
         }
-        // map has been built
-        boolean equalOdd = false;
+
+        // checking for same char
         for(String st : map.keySet()){
             char ch1 = st.charAt(0);
             char ch2 = st.charAt(1);
-            String s = String.valueOf(ch2) + String.valueOf(ch1);
-            if(map.containsKey(s) && ch1 != ch2){
-                int c = Math.min(map.get(s),map.get(st));
-                ans += 2*c;
-            } else if(ch1 == ch2){
-                // aa type string 
-                int c = map.get(st);
-                if(c%2 == 0){
-                    ans += 2*c;
-                } else {
-                    equalOdd=true;
-                    ans += 2*(c-1);
-                }
-            }
+            if(ch1 == ch2 && map.get(st) > 0){
+                ans+=2;
+                map.put(st,map.get(st)-1);
+                break;
+            } 
         }
-        // now I want to see those values who have symmetry xx yy ll
-        
-        if(equalOdd) ans += 2;
         return ans;
     }
 }
