4160847fcc1ab8407cc4293978abfb4c8d9fb693
Mohd Ajeem <149220624+mohdajeem@users.noreply.github.com>
2025-05-27 23:53:47 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (53.59%) - LeetSync

--- 3172-divisible-and-non-divisible-sums-difference/divisible-and-non-divisible-sums-difference.java
+++ 3172-divisible-and-non-divisible-sums-difference/divisible-and-non-divisible-sums-difference.java
@@ -1,13 +1,16 @@
 class Solution {
     public int differenceOfSums(int n, int m) {
-        int num1 = 0, num2 = 0;
-        for(int i =1;i<=n;i++){
-            if(i % m == 0){
-                num2+=i;
-            } else {
-                num1+=i;
-            }
-        }
-        return num1-num2;
+        // sum(1-n)/2 -> gives sum upto n -> n*(n+1)/2 gives sum
+        // now there are those values those are div by m
+        // now to reduce them i will use 
+        // see -> total no. till n that are div. by m => k = (n/m) call it k
+        // 1*m + 2*m + 3*m +...k*m -> m(1+2+3+...k) => n2 = m*(k*(k+1)/2)
+        // now i want to remove n2 from the value that aleady contain 1-n sum exclude n2
+        // sum(1-n)-n2 -n2;
+        // sum(1-n) - 2*n2;
+        // return sum(1-n) - m*k*(k+1);
+        int k = n/m;
+        return (n*(n+1))/2 - m*k*(k+1);
+
     }
 }
