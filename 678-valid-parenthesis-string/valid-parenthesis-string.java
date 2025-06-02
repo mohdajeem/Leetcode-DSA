a26698278794e51b313fbba51043685d0444ce10
Mohd Ajeem <149220624+mohdajeem@users.noreply.github.com>
2025-06-02 17:48:18 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (78.58%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.java
+++ 678-valid-parenthesis-string/valid-parenthesis-string.java
@@ -1,22 +1,21 @@
 class Solution {
     public boolean checkValidString(String s) {
-        int n = s.length();
-        Boolean[][] dp = new Boolean[n][n];
-        return fxn(0, 0, s,dp);
-    }
-
-    public boolean fxn(int ind, int count, String s, Boolean[][] dp) {
-        if(count < 0) return false;
-        if(ind == s.length()){
-            return count == 0;
+        int min =0, max =0;
+        for(int i =0;i<s.length();i++){
+            char ch = s.charAt(i);
+            if(ch == '('){
+                min++;
+                max++;
+            } else if(ch == ')'){
+                min--;
+                max--;
+            } else{
+                min--;
+                max++;
+            }
+            if(max <  0) return false;
+            if(min < 0) min =0;
         }
-        if(dp[ind][count]!= null) return dp[ind][count];
-        if(s.charAt(ind) == '('){
-            return dp[ind][count]=fxn(ind+1,count+1,s,dp);
-        } else if(s.charAt(ind) == ')'){
-            return dp[ind][count]=fxn(ind+1,count-1,s,dp);
-        }
-
-        return dp[ind][count]=fxn(ind+1,count+1,s,dp) || fxn(ind+1,count,s,dp) || fxn(ind+1,count-1,s,dp);
+        return min == 0;
     }
 }
