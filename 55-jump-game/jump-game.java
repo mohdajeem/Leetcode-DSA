a4b035b937f25ecc0a5731e8342b9244acfa8c9c
Mohd Ajeem <149220624+mohdajeem@users.noreply.github.com>
2025-06-04 13:32:43 +0530

    Time: 3 ms (50.84%) | Memory: 46.4 MB (8.58%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,19 +1,10 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        Boolean[] dp = new Boolean[nums.length];
-        return canJumpRec(0,nums,dp);
-    }
-    public boolean canJumpRec(int ind, int[] nums, Boolean[] dp){
-        if(ind >= nums.length-1){
-            return true;
+        int maxInd = 0;
+        for(int i =0;i<nums.length;i++){
+            if(i > maxInd) return false;
+            maxInd = Math.max(maxInd, nums[i]+i);
         }
-        if(nums[ind] == 0) return false;
-        if(dp[ind] != null) return dp[ind];
-        boolean res = false;
-        for(int i = 1;i<=nums[ind];i++){
-            res = res || canJumpRec(ind+i,nums,dp);
-        }
-        return dp[ind]=res;
-
+        return true;
     }
 }
