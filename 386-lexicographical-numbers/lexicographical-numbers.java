51100e8fcafb687d33f275afb993c177cf21ebde
Mohd Ajeem <149220624+mohdajeem@users.noreply.github.com>
2025-06-08 20:37:25 +0530

    Time: 1 ms (100.00%) | Memory: 48.4 MB (69.78%) - LeetSync

--- 386-lexicographical-numbers/lexicographical-numbers.java
+++ 386-lexicographical-numbers/lexicographical-numbers.java
@@ -1,15 +1,19 @@
 class Solution {
-    HashSet<Integer> set = new HashSet<>();
     public List<Integer> lexicalOrder(int n) {
         List<Integer> ans = new ArrayList<>();
-        for(int i =1;i<10;i++) rec(i, n, ans);
+        for(int i = 1;i<=9;i++){
+            dfs(i, n, ans);
+        }
         return ans;
     }
-    public void rec(int i, int n, List<Integer> ans){
+    public void dfs(int i, int n, List<Integer> ans){
         if(i > n) return;
         ans.add(i);
-        for(int j = i*10;j<i*10+10;j++){
-            rec(j,n,ans);
-        }        
+        for(int j = 0;j<=9;j++){
+            int v = 10*i + j;
+            if(v > n) return;
+            dfs(v,n, ans);
+        }
     }
+
 }
