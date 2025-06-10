166ebf963874d75d4875535f17ca19ce22885f91
Mohd Ajeem <149220624+mohdajeem@users.noreply.github.com>
2025-06-10 18:19:01 +0530

    Time: 3 ms (92.79%) | Memory: 46.7 MB (44.78%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
@@ -1,34 +1,21 @@
 class Solution {
     public int longestOnes(int[] nums, int k) {
-        // queue
-        // if 0 - c++, and if(c>k) remove and 0 c--
-        int c = 0;
-        int n = nums.length;
-        Queue<Integer> q = new LinkedList<>();
-        int ans =0;
-        for(int i : nums){
-            if(i == 0){
-                ans = Math.max(ans,q.size());
-                if(c == k){
-                    while(!q.isEmpty() && q.peek() != 0){
-                        q.remove();
-                    }
-                    if(!q.isEmpty()) {
-                        q.remove();
-                        c--;
-                    }
-                }
-                if(c<k){
-                    q.add(i);
-                    c++;
-                }
-                
-            } else{
-                q.add(i);
+        int sum =0;
+        int l = 0, r = 0, n = nums.length, c =0;
+        int max = 0;
+        while(r<n){
+            sum++;
+            if(nums[r]==0){
+                c++;
             }
-            
+            while(l<=r && c > k){
+                if(nums[l]==0) c--;
+                sum--;
+                l++;
+            }
+            r++;
+            max = Math.max(sum, max);
         }
-        ans = Math.max(ans,q.size());
-        return ans;
+        return max;
     }
 }
