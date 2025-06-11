93cf23ae16d625264ea72302e9b38957544c90ae
Mohd Ajeem <149220624+mohdajeem@users.noreply.github.com>
2025-06-11 14:51:23 +0530

    Time: 4 ms (23.61%) | Memory: 53.7 MB (74.07%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.java
+++ 1039-find-the-town-judge/find-the-town-judge.java
@@ -1,23 +1,11 @@
 class Solution {
     public int findJudge(int n, int[][] trust) {
-        // if cyclic -> return -1;
-        // i will use topo sort -> by using bfs 
-        // i will use reverse of directions
-        // u->v -> adj.get(v).add(u);
-        // then who will be the first will be the answer after topo sort
-        // queue<Pair> store;
-        // 
-        List<List<Integer>> adj = new ArrayList<>();
-        for(int i =0;i<n;i++){
-            adj.add(new ArrayList<>());
-        }
         int[] ind = new int[n];
         int[] out = new int[n];
         List<Integer> ans = new ArrayList<>();
         for(int i =0;i<trust.length;i++){
             int u = trust[i][0]-1;
             int v = trust[i][1]-1;
-            adj.get(v).add(u);
             ind[v]++;
             out[u]++;
         }
