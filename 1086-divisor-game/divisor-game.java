1931d900410b423c10bcccf50492d97637acaeef
Mohd Ajeem <149220624+mohdajeem@users.noreply.github.com>
2025-06-12 12:25:23 +0530

    Time: 4 ms (3.51%) | Memory: 42.4 MB (6.26%) - LeetSync

--- 1086-divisor-game/divisor-game.java
+++ 1086-divisor-game/divisor-game.java
@@ -1,18 +1,31 @@
 class Solution {
     public boolean divisorGame(int n) {
-        // return (n&1) == 0;
-        return rec(n, 0);
+        HashMap<Integer, Boolean> map = new HashMap<>();
+        return memo(n, map);
     }
-    public boolean rec(int n, int p){
-        if(n == 1){
-            if(p == 0) return false;
-            return true;
+    public boolean memo(int n, HashMap<Integer, Boolean> map){
+        // momoize 
+        if(map.containsKey(n)){
+            return map.get(n);
         }
-        if(n == 2){
-            if(p==0) return true;
-            return false;
+        if(n == 1) return false;
+        List<Integer> factors = new ArrayList<>();
+        factors.add(1);
+        for(int i=2;i*i<=n;i++){
+            if(n %i == 0){
+                factors.add(i);
+                if(i != (n/i)){
+                    factors.add(n/i);
+                }
+            }
         }
-        return p == 1 ? rec(n-1, 0) : rec(n-1, 1);
+        for(int f : factors){
+            if(!memo(n-f, map)){
+                map.put(n, true);
+                return true;
+            }
+        }
+        map.put(n, false);
+        return false;
     }
-
 }
