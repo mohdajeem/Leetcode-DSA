5648e1d871affe8a2226991f96017d7e242f34c7
Mohd Ajeem <149220624+mohdajeem@users.noreply.github.com>
2025-06-12 12:14:05 +0530

    Time: 0 ms (100.00%) | Memory: 40.1 MB (95.88%) - LeetSync

--- 1086-divisor-game/divisor-game.java
+++ 1086-divisor-game/divisor-game.java
@@ -1,6 +1,18 @@
 class Solution {
     public boolean divisorGame(int n) {
-        // return false;
-        return (n&1) == 0;
+        // return (n&1) == 0;
+        return rec(n, 0);
     }
+    public boolean rec(int n, int p){
+        if(n == 1){
+            if(p == 0) return false;
+            return true;
+        }
+        if(n == 2){
+            if(p==0) return true;
+            return false;
+        }
+        return p == 1 ? rec(n-1, 0) : rec(n-1, 1);
+    }
+
 }
