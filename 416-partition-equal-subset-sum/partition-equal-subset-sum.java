ece2e518dc670f2b07bdd01a3f49f6fe82dec8b0
Mohd Ajeem <149220624+mohdajeem@users.noreply.github.com>
2025-06-14 12:51:12 +0530

    Time: 90 ms (24.52%) | Memory: 57.9 MB (15.78%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,54 +1,22 @@
 class Solution {
     public boolean canPartition(int[] nums) {
+        int totSum = 0;
+        for(int i : nums) totSum+=i;
+        if((totSum & 1) == 1) return false;
+        int target = totSum/2;
         int n = nums.length;
-        int sum = 0;
-        for(int i = 0;i<n;i++){
-            sum+=nums[i];
-        }
-        if(sum%2 != 0) return false;
-        // return subsetSumEqual(n-1, sum/2, nums);
-        // Now we will do memoization in this function
-        int t = sum/2;
-        int[][] dp = new int[n][t+1];
-        for(int[] ar: dp) Arrays.fill(ar,-1);
-        // return subsetSumEqual(n-1, t, nums, dp);
-        return tabulation(nums, n, sum);
-        
+        Boolean[][] dp = new Boolean[n][target+1];
+        return rec(n-1, target, nums, dp);
     }
-    public boolean tabulation(int[] nums, int n, int s){
-        int t = s/2;
-        boolean[][] dp = new boolean[n][t+1];
-        // dp[0][nums[0]] = true;
-        for(int i = 0;i<n;i++){
-            dp[i][0] = true;
-        }
-        for(int ind = 1;ind<n;ind++){
-            for(int  tar = 1;tar<=t;tar++){
-                boolean notTake = dp[ind-1][tar];
-                boolean take = false;
-                if(nums[ind] <= tar){
-                    take = dp[ind-1][tar-nums[ind]];
-                }
-                dp[ind][tar] = take || notTake;
-            }
-        }
-        return dp[n-1][t];
-    }
-
-    public boolean subsetSumEqual(int ind, int t, int[] nums,int[][] dp){
-
-        // base Case 
-        if(ind == 0){
-            return t == nums[ind];
-        }
-        if(t==0) return true;
-        if(dp[ind][t] != -1) return dp[ind][t] == 1;
-        boolean notTake = subsetSumEqual(ind-1, t, nums,dp);
+    public boolean rec(int ind, int tar, int[] nums, Boolean[][] dp){
+        if(tar == 0) return true;
+        if(ind < 0) return false;
+        if(dp[ind][tar] != null) return dp[ind][tar];
+        boolean notTake = rec(ind-1, tar, nums, dp);
         boolean take = false;
-        if(nums[ind] <= t){
-            take = subsetSumEqual(ind-1, t-nums[ind], nums,dp);
+        if(nums[ind] <= tar){
+            take = rec(ind-1, tar - nums[ind], nums, dp);
         }
-        dp[ind][t] = (take||notTake) ? 1 : 0;
-        return take || notTake;
+        return dp[ind][tar] = take || notTake;
     }
 }
