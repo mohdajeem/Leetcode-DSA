d9f14121631d5bea1b6679f2dc30018c2a41036f
Mohd Ajeem <149220624+mohdajeem@users.noreply.github.com>
2025-06-27 19:15:49 +0530

    Time: 833 ms (11.42%) | Memory: 55.4 MB (53.41%) - LeetSync

--- 2150-kth-smallest-product-of-two-sorted-arrays/kth-smallest-product-of-two-sorted-arrays.java
+++ 2150-kth-smallest-product-of-two-sorted-arrays/kth-smallest-product-of-two-sorted-arrays.java
@@ -1,36 +1,35 @@
 class Solution {
     public long kthSmallestProduct(int[] nums1, int[] nums2, long k) {
-        long s = -10000000000L, e = 10000000000L;
-        long ans = 0;
-        while(s <= e){
-            long mid = (s+e)/2;
+        long low = -10000000000L, high = 10000000000L;
+        int n = nums1.length;
+        while(low <= high){
+            long mid = (low+high)/2;
             long count = 0;
-            for(int i = 0;i<nums1.length;i++){
-                count += getCount(mid, nums1[i], nums2);
+            for(int i = 0;i<n;i++){
+                count += fxn(nums1[i], mid,nums2);
             }
             if(count < k){
-                s = mid+1;
+                low = mid+1;
             } else{
-                e = mid-1;
+                high = mid -1;
             }
         }
-        return s;
+        return low;
     }
-    public long getCount(long prod, int x1, int[] b){
-        int n2 = b.length;
-        int l = 0, r = n2-1;
-        while(l<=r){
-            int mid = (l+r)/2;
-            long product = 1L*x1*b[mid];
-            if((x1 >= 0 && product <= prod) || (x1 < 0 && product > prod)){
+    public long fxn(int x, long prod, int[] nums2){
+        int n2 = nums2.length;
+        int l = 0, h = n2-1;
+        while(l<=h){
+            int mid = (l+h)/2;
+            long pr = 1L*x*nums2[mid];
+            if((x >= 0 && pr <= prod) || (x<0 && pr > prod) ){
                 l = mid+1;
             } else{
-                r = mid-1;
+                h = mid-1;
             }
         }
-        if(x1 >= 0) return l;
+        if(x >= 0) return l;
         return n2 - l;
+
     }
-
-
 }
