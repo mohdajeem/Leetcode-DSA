7cd33af32725b8bb75e9de11d83a448af41aa38f
Mohd Ajeem <149220624+mohdajeem@users.noreply.github.com>
2025-06-28 21:28:52 +0530

    Time: 1 ms (98.24%) | Memory: 41.2 MB (30.04%) - LeetSync

--- 441-arranging-coins/arranging-coins.java
+++ 441-arranging-coins/arranging-coins.java
@@ -1,10 +1,36 @@
 class Solution {
     public int arrangeCoins(int n) {
-        return rec(1, n);
+        long l = 0, h = n;
+        while(l<=h){
+            long mid = l+(h-l)/2;
+            long val = (mid*(mid+1))/2;
+            if(val == n) return (int) mid;
+            else if(val > n) h = mid-1;
+            else l = mid+1;
+        }
+        return (int)h;
+
+        /*
+        long sqt = sqrt((long)(8*n+1));
+        // int res = (int)Math.min(Math.abs((-1+sqt)/2), Math.abs((-1-sqt)/2));
+        int res = (int) Math.floor((Math.abs((-1+sqt)/2)));
+        return res;
+        */
     }
-    public int rec(int i, int n){
-        if(i > n) return 0;
-        
-        return 1 + rec(i+1, n-i);
+    /*
+    public long sqrt(long n){
+        long s = 1, e = n/2;
+        long ans = 0;
+        while(s <= e){
+            long mid = s + (e-s)/2;
+            if(mid*mid <= n){
+                ans = mid;
+                s = mid+1;
+            } else{
+                e = mid-1;
+            }
+        }
+        return ans;
     }
+    */
 }
