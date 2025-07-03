1dede1779fc8ec5346db243c77f238f2d75cfdce
Mohd Ajeem <149220624+mohdajeem@users.noreply.github.com>
2025-07-03 16:47:20 +0530

    Time: 11 ms (92.39%) | Memory: 48.9 MB (44.07%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,19 +1,28 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        if(k >= nums.length) return nums;
+        int n = nums.length;
         HashMap<Integer, Integer> map = new HashMap<>();
-        for(int i : nums){
-            map.put(i, map.getOrDefault(i,0)+1);
+        for(int i : nums) map.put(i, map.getOrDefault(i,0)+1);
+        List<List<Integer>> freq = new ArrayList<>();
+        for(int i =0;i<nums.length+1;i++){
+            freq.add(new ArrayList<>());
         }
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)-> Integer.compare(b[1],a[1]));
         for(int key : map.keySet()){
-            int[] arr = {key, map.get(key)};
-            pq.add(arr);
+            int fr = map.get(key);
+            freq.get(fr).add(key);
         }
         int[] ans = new int[k];
         int j =0;
-        for(int i =0;i<k;i++){
-            ans[j++] = pq.remove()[0];
+        for(int i = n;i>=0;i--){
+            if(freq.get(i).size() != 0){
+                int l =0;
+                while(k>0 && l<freq.get(i).size()){
+                    ans[j++] = freq.get(i).get(l);
+                    l++;
+                    k--;
+                }
+                if(k == 0) return ans;
+            }
         }
         return ans;
     }
