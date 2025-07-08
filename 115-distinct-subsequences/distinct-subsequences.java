c134bd90b61d36d2e960e89b8f95f50a0a88f7eb
Mohd Ajeem <149220624+mohdajeem@users.noreply.github.com>
2025-07-08 18:38:23 +0530

    Time: 20 ms (41.24%) | Memory: 51.8 MB (10.58%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -1,9 +1,26 @@
 class Solution {
     public int numDistinct(String s, String t) {
         int n = s.length(), m = t.length();
+        return tabulation(n,m, s, t);
+        // int[][] dp = new int[n+1][m+1];
+        // for(int[] ar : dp) Arrays.fill(ar, -1);
+        // return rec(0,0, s, t, dp);
+    }
+    public int tabulation(int n, int m, String s, String t){
         int[][] dp = new int[n+1][m+1];
-        for(int[] ar : dp) Arrays.fill(ar, -1);
-        return rec(0,0, s, t, dp);
+        for(int i = 0;i<=n;i++){
+            dp[i][0]=1;
+        }
+        for(int i = 1;i<=n;i++){
+            for(int j = 1;j<=m;j++){
+                int val = dp[i-1][j];
+                if(s.charAt(i-1) == t.charAt(j-1)){
+                    val += dp[i-1][j-1];
+                }
+                dp[i][j] = val;
+            }
+        }
+        return dp[n][m];
     }
     public int rec(int i, int j, String s, String t, int[][] dp){
         if(j == t.length()) return 1;
