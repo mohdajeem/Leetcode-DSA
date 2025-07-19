7baee7ace99cf10a6815bb7cfc613f9e2c1b342d
Mohd Ajeem <149220624+mohdajeem@users.noreply.github.com>
2025-07-19 17:40:29 +0530

    Time: 3 ms (12.26%) | Memory: 49 MB (6.84%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -1,10 +1,8 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int n = prices.length;
-        return tabu(n,prices);
-        // int[][] dp = new int[n][2];
-        // for(int[] ar : dp)  Arrays.fill(ar, -1);
-        // return rec(0,0,prices,dp);
+        int[][] dp = new int[prices.length][2];
+        for(int[] ar : dp) Arrays.fill(ar, -1);
+        return rec(0,0, prices, dp);
     }
     public int rec(int ind, int buy, int[] prices, int[][] dp){
         if(ind == prices.length) return 0;
@@ -13,29 +11,11 @@ class Solution {
 
         int profit = 0;
         if(buy == 0){
-            profit = Math.max(-prices[ind]+rec(ind+1,1,prices,dp), 0+rec(ind+1, 0, prices,dp));
+            profit = Math.max(-prices[ind]+rec(ind+1, 1, prices,dp), rec(ind+1, 0, prices,dp));
         }
         if(buy == 1){
-            profit = Math.max(prices[ind]+rec(ind+1, 0, prices,dp), 0+rec(ind+1, 1, prices,dp));
+            profit = Math.max(prices[ind]+rec(ind+1, 0, prices,dp), rec(ind+1, 1, prices,dp));
         }
         return dp[ind][buy] = profit;
-
-    }
-    public int tabu(int n, int[] prices){
-        int[][] dp = new int[n+1][2];
-
-        for(int i = n-1;i>=0;i--){
-            for(int j = 0;j<=1;j++){
-                int profit = 0;
-                if(j == 0){
-                    profit = Math.max(-prices[i]+dp[i+1][1], 0+dp[i+1][0]);
-                }
-                if(j == 1){
-                    profit = Math.max(prices[i]+dp[i+1][0], 0+dp[i+1][1]);
-                }
-                dp[i][j] = profit;
-            }
-        }
-        return dp[0][0];
     }
 }
