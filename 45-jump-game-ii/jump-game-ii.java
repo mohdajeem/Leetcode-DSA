57cc52ad772505a9828612d5268178234e1102da
Mohd Ajeem <149220624+mohdajeem@users.noreply.github.com>
2025-07-20 23:06:43 +0530

    Time: 124 ms (7.17%) | Memory: 46.3 MB (6.24%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -1,18 +1,17 @@
 class Solution {
     public int jump(int[] nums) {
-        int n = nums.length;
-        int[] dp = new int[n];
+        int[] dp =  new int[nums.length];
         Arrays.fill(dp,-1);
-        return rec(0,nums,dp);
+        return rec(0, nums, dp);
     }
-    public int rec(int ind, int[] nums,int[] dp){
+    public int rec(int ind, int[] nums, int[] dp){
         if(ind >= nums.length-1) return 0;
-        if(nums[ind] == 0) return (int)1e7;
-        int take = (int)1e7;
         if(dp[ind] != -1) return dp[ind];
-        for(int j=1;j<=nums[ind];j++){
-            take = Math.min(take,1+rec(ind+j,nums,dp));
+        if(nums[ind] == 0) return (int)1e9;
+        int min = (int)1e9;
+        for(int i = 1;i<=nums[ind];i++){
+            min = Math.min(min, 1+rec(ind+i, nums, dp));
         }
-        return dp[ind]=take;
+        return dp[ind] = min;
     }
 }
