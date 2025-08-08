13a78b5248c44cf97e4bb400b3fdd237e9078745
Mohd Ajeem <149220624+mohdajeem@users.noreply.github.com>
2025-08-08 13:18:53 +0530

    Time: 7 ms (68.80%) | Memory: 44.8 MB (54.27%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
@@ -1,9 +1,30 @@
 class Solution {
     public int minDistance(String word1, String word2) {
         int n = word1.length(), m = word2.length();
-        int[][] dp = new int[n][m];
-        for(int[] ar : dp) Arrays.fill(ar, -1);
-        return rec(word1, word2, 0,0, dp);
+        return tabu(word1, word2, n, m);
+        // int[][] dp = new int[n][m];
+        // for(int[] ar : dp) Arrays.fill(ar, -1);
+        // return rec(word1, word2, 0,0, dp);
+    }
+    public int tabu(String word1, String word2, int n, int m){
+        int[][] dp = new int[n+1][m+1];
+        for(int j = 0;j<=m;j++){
+            dp[n][j]=m-j;
+        }
+        for(int i =0;i<=n;i++){
+            dp[i][m] = n-i;
+        }
+
+        for(int i = n-1;i>=0;i--){
+            for(int j = m-1;j>=0;j--){
+                if(word1.charAt(i) == word2.charAt(j)){
+                    dp[i][j] = dp[i+1][j+1];
+                } else{
+                    dp[i][j] = 1 + Math.min(dp[i+1][j], dp[i][j+1]);
+                }
+            }
+        }
+        return dp[0][0];
     }
     public int rec(String word1, String word2, int i, int j, int[][] dp){
         if(i == word1.length()){
