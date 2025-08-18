456a53c6a2f32dbc571b071689f506f935571bd5
Mohd Ajeem <149220624+mohdajeem@users.noreply.github.com>
2025-08-18 11:20:41 +0530

    Time: 3 ms (28.09%) | Memory: 45.3 MB (84.93%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -1,69 +1,56 @@
 class Solution {
     public void solve(char[][] board) {
-        // vis[][] array - boolean
-        // array - track O's ka surrounding
-        // make a function of bfs 
         int n = board.length, m = board[0].length;
         boolean[][] vis = new boolean[n][m];
-        int[][] arr=new int[n][m];
-        for(int i =0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if((i==0||i==n-1)){
-                    if(!vis[i][j] && board[i][j] == 'O'){
-                        bfs(i,j,n,m,vis,arr,board);
-                    }
-                } else{
-                    if(j==0 || j == m-1){
-                        if(!vis[i][j] && board[i][j]=='O'){
-                            bfs(i,j,n,m,vis,arr,board);
-                        }
-                    }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if ((i == 0 || i == n - 1) && (board[i][j] == 'O') ) {
+                    bfs(i, j, vis, board, n, m);
+                } else if ((j == 0 || j == m - 1) && (board[i][j] == 'O')) {
+                    bfs(i, j, vis, board, n, m);
                 }
-                
             }
         }
-        for(int[] ar:arr){
-            System.out.println(Arrays.toString(ar));
-        }
-        // now I have arr and board
-        for(int i=0;i<n;i++){
+        for(int i =0;i<n;i++){
             for(int j =0;j<m;j++){
-                if(board[i][j] == 'O' && arr[i][j] != 1){
-                    board[i][j]='X';
+                if(board[i][j] == 'O' && vis[i][j]==false){
+                    board[i][j] = 'X';
                 }
             }
         }
     }
 
-    public void bfs(int i, int j, int n,int m, boolean[][] vis, int[][] arr, char[][] board){
-        Queue<Pair> q = new LinkedList<>();
+    public void bfs(int i, int j, boolean[][] vis, char[][] board, int n, int m) {
         vis[i][j] = true;
-        arr[i][j] = 1;
-        q.add(new Pair(i,j));
-        while(!q.isEmpty()){
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(i, j));
+        int[] drow = { 0, -1, 0, 1 };
+        int[] dcol = { 1, 0, -1, 0 };
+        while (!q.isEmpty()) {
             Pair p = q.remove();
             int row = p.row;
             int col = p.col;
-            int[] drow = {0,-1,0,1};
-            int[] dcol = {1,0,-1,0};
-            for(int z =0;z<4;z++){
-                int nrow = row+drow[z];
-                int ncol = col+dcol[z];
-                if(nrow >=0 && nrow < n && ncol>=0 && ncol < m && !vis[nrow][ncol] && board[nrow][ncol] == 'O'){
+            for (int k = 0; k < 4; k++) {
+                int nrow = row + drow[k];
+                int ncol = col + dcol[k];
+                if (isValid(nrow, ncol, n, m) && !vis[nrow][ncol] && board[nrow][ncol] == 'O') {
                     vis[nrow][ncol] = true;
-                    arr[nrow][ncol] = 1;
-                    q.add(new Pair(nrow,ncol));
+                    q.add(new Pair(nrow, ncol));
                 }
             }
         }
     }
+
+    public boolean isValid(int i, int j, int n, int m) {
+        return i >= 0 && i < n && j >= 0 && j < m;
+    }
 }
 
-class Pair{
-    int row;
-    int col;
-    public Pair(int row, int col){
-        this.row=row;
-        this.col=col;
+class Pair {
+    int row, col;
+
+    Pair(int row, int col) {
+        this.row = row;
+        this.col = col;
     }
 }
