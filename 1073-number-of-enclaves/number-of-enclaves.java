58842e359038e7715ed214d1021d82869034fe22
Mohd Ajeem <149220624+mohdajeem@users.noreply.github.com>
2025-08-18 12:47:57 +0530

    Time: 8 ms (73.72%) | Memory: 54.3 MB (78.56%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.java
+++ 1073-number-of-enclaves/number-of-enclaves.java
@@ -2,59 +2,55 @@ class Solution {
     public int numEnclaves(int[][] grid) {
         int n = grid.length, m = grid[0].length;
         boolean[][] vis = new boolean[n][m];
-        int c=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(i == 0 || i == n-1){
-                    if(!vis[i][j] && grid[i][j] == 1){
-                        bfs(i,j,n,m,vis,grid);
-                    }
-                } else{
-                    if(j==0 || j==m-1){
-                        if(!vis[i][j] && grid[i][j] == 1){
-                            bfs(i,j,n,m,vis,grid);
-                        }
+        int c =0;
+        for(int i =0;i<n;i++){
+            for(int j =0;j<m;j++){
+                if(i == 0 || i == n-1 || j == 0 || j == m-1){
+                    if(grid[i][j] == 1 && !vis[i][j]){
+                        bfs(i,j,vis,grid,n,m);
                     }
                 }
             }
         }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]==1){
+        for(int i =0;i<n;i++){
+            for(int j =0;j<m;j++){
+                if(grid[i][j] == 1 && !vis[i][j]){
                     c++;
                 }
             }
         }
         return c;
+
     }
-    public void bfs(int sr,int sc,int n, int m, boolean[][] vis, int[][] grid){
-        vis[sr][sc] = true;
+    public void bfs(int i, int j, boolean[][] vis, int[][] grid, int n, int m){
+        vis[i][j] = true;
         Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(sr,sc));
-        grid[sr][sc] = 0;
+        q.add(new Pair(i,j));
+        int[] drow = {0,-1,0,1};
+        int[] dcol = {1,0,-1,0};
         while(!q.isEmpty()){
             Pair p = q.remove();
             int row = p.row;
             int col = p.col;
-            int[] drow = {0,-1,0,1};
-            int[] dcol = {1,0,-1,0};
-            for(int i =0;i<4;i++){
-                int nrow = row+drow[i];
-                int ncol = col+dcol[i];
-                if(nrow >=0 && nrow<n && ncol >= 0 && ncol < m && !vis[nrow][ncol] && grid[nrow][ncol] == 1){
-                    grid[nrow][ncol]=0;
-                    vis[nrow][ncol]=true;
-                    q.add(new Pair(nrow,ncol));
+            for(int k =0;k<4;k++){
+                int nrow = row+drow[k];
+                int ncol = col + dcol[k];
+                if(isValid(nrow, ncol, n, m) && !vis[nrow][ncol] && grid[nrow][ncol] == 1){
+                    vis[nrow][ncol] = true;
+                    q.add(new Pair(nrow, ncol));
                 }
             }
         }
     }
+    public boolean isValid(int i, int j, int n, int m){
+        return i >=0 && i<n && j >=0 && j<m;
+    }
 }
+
 class Pair{
-    int row;
-    int col;
-    public Pair(int row, int col){
-        this.row=row;
-        this.col=col;
+    int row, col;
+    Pair(int row, int col){
+        this.row = row;
+        this.col = col;
     }
 }
