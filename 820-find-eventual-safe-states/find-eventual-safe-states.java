9e2d6aa307e1971bbb3fe1d4a1f839842adfc73b
Mohd Ajeem <149220624+mohdajeem@users.noreply.github.com>
2025-08-19 12:39:11 +0530

    Time: 26 ms (25.63%) | Memory: 55.6 MB (57.06%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.java
+++ 820-find-eventual-safe-states/find-eventual-safe-states.java
@@ -1,36 +1,31 @@
 class Solution {
     public List<Integer> eventualSafeNodes(int[][] graph) {
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
         int n = graph.length;
-        for(int i = 0;i<graph.length;i++){
-            adj.add(new ArrayList<>());
-        }
-        for(int i = 0;i<n;i++){
-            for(int j = 0;j<graph[i].length;j++){
+        int[] outDegree = new int[n];
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i =0;i<n;i++) adj.add(new ArrayList<>());
+
+        for(int i =0;i<n;i++){
+            for(int j =0;j<graph[i].length;j++){
                 adj.get(graph[i][j]).add(i);
             }
         }
-        int[] ind = new int[n];
-        for(int i = 0;i<adj.size();i++){
-            for(int it : adj.get(i)){
-                ind[it]++;
-            }
-        }
-        // steps for topological sort 
-        // q me ind == 0 gusa dunga
-        // ind -- (for each it: queue se nikalunga)
+
+        for(int i=0;i<n;i++) outDegree[i] = graph[i].length;
         Queue<Integer> q = new LinkedList<>();
-        for(int i = 0;i<n;i++){
-            if(ind[i]==0) q.add(i);
+        for(int i =0;i<n;i++){
+            if(outDegree[i] == 0) q.add(i);
         }
         List<Integer> ans = new ArrayList<>();
         while(!q.isEmpty()){
-            int i = q.remove();
-            for(int it : adj.get(i)){
-                ind[it]--;
-                if(ind[it]==0) q.add(it);
+            int node = q.remove();
+            ans.add(node);
+            for(int it : adj.get(node)){
+                outDegree[it]--;
+                if(outDegree[it]==0){
+                    q.add(it);
+                }
             }
-            ans.add(i);
         }
         Collections.sort(ans);
         return ans;
