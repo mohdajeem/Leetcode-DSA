cdb4d72b2b11b8e87b4d3dc7faa5383f783eb2ad
Mohd Ajeem <149220624+mohdajeem@users.noreply.github.com>
2025-08-26 13:03:43 +0530

    Time: 0 ms (100.00%) | Memory: 47.6 MB (64.05%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.java
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.java
@@ -13,42 +13,11 @@
  *     }
  * }
  */
-
 class Solution {
     public int countNodes(TreeNode root) {
-        HashSet<TreeNode> vis = new HashSet<>();
-        // Queue<Pair> q = new Queue<>();
-        // q.add(new Pair(root,1));
         if(root == null) return 0;
-        Queue<TreeNode> q = new LinkedList<>();
-        int c = 1;
-        q.add(root);
-        while(!q.isEmpty()){
-            // Pair p = new Pair
-            TreeNode node = q.remove();
-            if(node.left != null){
-                c++;
-                q.add(node.left);
-            }
-            if(node.right != null){
-                c++;
-                q.add(node.right);
-            }
-        }
-        return c;
-
-
-
-
-
-    }
-}
-
-class Pair{
-    TreeNode node;
-    int n;
-    public Pair(TreeNode node, int n){
-        this.node = node;
-        this.n =n;
+        int left = countNodes(root.left);
+        int right = countNodes(root.right);
+        return 1 + left+right;
     }
 }
