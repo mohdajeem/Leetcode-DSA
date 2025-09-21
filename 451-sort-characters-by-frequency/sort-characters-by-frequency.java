136140810555c5fb7818bfaee403353602352620
Mohd Ajeem <149220624+mohdajeem@users.noreply.github.com>
2025-09-21 21:08:42 +0530

    Time: 13 ms (67.76%) | Memory: 46 MB (22.10%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.java
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.java
@@ -1,32 +1,31 @@
 class Solution {
     public String frequencySort(String s) {
-        HashMap<Character, Integer> freq = new HashMap<>();
-        PriorityQueue<Pair> pq = new PriorityQueue<>(
-            (obj1,obj2)-> {return obj1.fr-obj2.fr;}
-        );
+        Map<Character, Integer> map = new HashMap<>();
         for(char ch : s.toCharArray()){
-            freq.put(ch,freq.getOrDefault(ch,0)+1);
+            map.put(ch, map.getOrDefault(ch,0)+1);
         }
-        // freq created now time to sort it
-        for(char key : freq.keySet()){
-            int val = freq.get(key);
-            pq.add(new Pair(key,val));
+
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b) -> {
+            return b.freq - a.freq;
+        });
+        for(Map.Entry<Character,Integer> it : map.entrySet()){
+            pq.add(new Pair(it.getKey(), it.getValue()));
         }
         StringBuilder sb = new StringBuilder();
         while(!pq.isEmpty()){
             Pair p = pq.remove();
-            for(int i=0;i<p.fr;i++) sb.append(p.c);
+            for(int i =0;i<p.freq;i++){
+                sb.append(p.ch);
+            }
         }
-        return sb.reverse().toString();
+        return sb.toString();
     }
 }
-
 class Pair{
-    char c;
-    int fr;
-    public Pair(char c,int fr){
-        this.c=c;
-        this.fr=fr;
+    char ch;
+    int freq;
+    Pair(char ch, int freq){
+        this.ch = ch;
+        this.freq = freq;
     }
-
 }
